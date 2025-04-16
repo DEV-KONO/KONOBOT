8c6a137ef402ab60cf0304a463ad0ad4616e9931
jania20 <janiamsanchez@gmail.com>
2025-04-16 13:14:06 -0600

    librería en C para pasar los structs del array de tokens a json

--- CUSTOMLIBS/VOORHEES/Voorhees.h
+++ CUSTOMLIBS/VOORHEES/Voorhees.h
@@ -3,4 +3,18 @@
 
 //las funciones van aqui abajo declaradas, solo la primera linea, sigue a los otros archivos .h
 
+
+#include "C:/Users/samue/Documents/DEV/KONOBOTLLM/KONOBOT/CUSTOMLIBS/FILEREADER/Tolkien.h"  
+
+/**
+ * @brief Convierte un array de structs Tolkien a un archivo JSON.
+ * @param TolkienArray Array de estructuras Tolkien.
+ * @param tokenCount Número de tokens en el array (obtenido con WordCount).
+ * @param outputPath Ruta del archivo JSON de salida.
+ * @return 0 si éxito, -1 si error.
+*/
+int TolkienToJSON(Tolkien* TolkienArray, int tokenCount, const char* outputPath);
+
+
+
 #endif
