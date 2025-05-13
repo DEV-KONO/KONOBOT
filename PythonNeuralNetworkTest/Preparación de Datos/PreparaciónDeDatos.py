88ed9bf9870323ba1d6bb8bc46108c44a062ef6b
DEV-KONO <kono.devjourney@gmail.com>
2025-05-12 19:16:08 -0600

    tokens con embeddings listos
    agregamos los tokens <UNK> y <EOS> para representar palabras desconocidas y el final de una oración

--- PythonNeuralNetworkTest/Preparación de Datos/PreparaciónDeDatos.py
+++ PythonNeuralNetworkTest/Preparación de Datos/PreparaciónDeDatos.py
@@ -1,17 +1,33 @@
 import json
 
 #Guardamos el archivo .json en memoria
-with open(r"DATASETS\TOKENS\embedings.json", "r", encoding="utf-8") as t:
+with open(r"DATASETS\TOKENS\tokensWembedding\tokensWembedding.json", "r", encoding="utf-8") as t:
     tokens = json.load(t)
 
-#Hacemos un diccionario que guarde palabra: id y otro que guarde id: palabra
+#Hacemos 3 diccionarios, uno para guardar palabra:id, otro para guardar id:palabra y 
+#otro para guardar id:embedding
 
 word2id = {}
 id2word = {}
+embedding = {}
 
 for token in tokens["embeddings"]:
     word2id[f"{token["word"]}"] = token["token_id"]
     id2word[f"{token["token_id"]}"] = token["word"]
+    embedding[f"{token["token_id"]}"] = token["vector"]
 
-print(f"Word2ID['mamay'] = {word2id["mamay"]}")
-print(f"ID2Word[0] = {id2word["0"]}")
+with open(r"PythonNeuralNetworkTest\Preparación de Datos\Word2ID.json", "w", encoding="utf-8") as f:
+    f.write(json.dumps(word2id))
+
+with open(r"PythonNeuralNetworkTest\Preparación de Datos\ID2Word.json", "w", encoding="utf-8") as f:
+    f.write(json.dumps(id2word))
+
+with open(r"PythonNeuralNetworkTest\Preparación de Datos\embeddings.json", "w", encoding="utf-8") as f:
+    f.write(json.dumps(embedding))
+
+
+print(f"ID2Word['158965'] = {id2word["158965"]}")
+print(f"Embedding['158965'] = {embedding["158965"]}")
+
+print(f"ID2Word['158966'] = {id2word["158966"]}")
+print(f"Embedding['158966'] = {embedding["158966"]}")
